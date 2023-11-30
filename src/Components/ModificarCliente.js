a45348517159a39d774566fc25806cd3a645fcbd
fabian1013584223 <114703051+fabian1013584223@users.noreply.github.com>
2023-11-30 16:26:27 -0500

    Coreccion RegistrarPrecio creacion ModificarCliente

---
+++ src/Components/ModificarCliente.js
@@ -0,0 +1,132 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StatusBar, // Agrega la importación de StatusBar
+} from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+
+const ModificarCliente = () => {
+  const [tipoDocumento, setTipoDocumento] = useState();
+  const [numeroDocumento, setNumeroDocumento] = useState('');
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [correoElectronico, setCorreoElectronico] = useState('');
+  const [numeroTelefono, setNumeroTelefono] = useState('');
+
+  const handleModificarCliente = () => {
+    // Implementar la lógica para modificar el cliente aquí
+  };
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : null}
+    >
+      <ScrollView>
+        <View style={styles.form}>
+          <Text style={styles.label}>Tipo de documento</Text>
+          <DropDownPicker
+            items={[
+              { label: 'Identity card', value: 'Identity card' },
+              { label: 'Citizenship card', value: 'Citizenship card' },
+              { label: 'Foreigners card', value: 'Foreigners card' },
+              { label: 'Passport', value: 'Passport' },
+            ]}
+            defaultValue={tipoDocumento}
+            onChangeItem={(item) => setTipoDocumento(item.value)}
+            style={styles.dropdown}
+          />
+
+          <Text style={styles.label}>Documento</Text>
+          <TextInput
+            style={styles.input}
+            value={numeroDocumento}
+            onChangeText={setNumeroDocumento}
+            keyboardType="numeric"
+          />
+
+          <Text style={styles.label}>Nombre</Text>
+          <TextInput
+            style={styles.input}
+            value={nombre}
+            onChangeText={setNombre}
+          />
+
+          <Text style={styles.label}>Apellido</Text>
+          <TextInput
+            style={styles.input}
+            value={apellido}
+            onChangeText={setApellido}
+          />
+
+          <Text style={styles.label}>Correo electrónico</Text>
+          <TextInput
+            style={styles.input}
+            value={correoElectronico}
+            onChangeText={setCorreoElectronico}
+            keyboardType="email-address"
+          />
+
+          <Text style={styles.label}>Teléfono</Text>
+          <TextInput
+            style={styles.input}
+            value={numeroTelefono}
+            onChangeText={setNumeroTelefono}
+            keyboardType="phone-pad"
+          />
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={handleModificarCliente}
+          >
+            <Text style={styles.buttonText}>Continuar</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  form: {
+    marginHorizontal: 30,
+  },
+  label: {
+    fontSize: 14,
+    color: '#000',
+    marginBottom: 5,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    paddingBottom: 5,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#007bff',
+    paddingVertical: 15,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  dropdown: {
+    marginBottom: 20,
+  },
+});
+
+export default ModificarCliente;
--- src/Components/RegistrarPrecioScreen.js
+++ src/Components/RegistrarPrecioScreen.js
@@ -1,71 +1,84 @@
 import React, { useState } from 'react';
 import {
- View,
- Text,
- StyleSheet,
- TextInput,
- Button,
- TouchableOpacity,
- ScrollView,
- Alert,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  ScrollView,
+  Alert,
+  Picker, // Asegúrate de importar Picker de react-native
 } from 'react-native';
 
 const RegistrarPrecio = () => {
- const [producto, setProducto] = useState('');
- const [precioCompra, setPrecioCompra] = useState('');
- const [precioVenta, setPrecioVenta] = useState('');
- const [descuento, setDescuento] = useState('');
- const [fechaDescuento, setFechaDescuento] = useState('');
- const [estadoDescuento, setEstadoDescuento] = useState('');
+  const [producto, setProducto] = useState('');
+  const [precioCompra, setPrecioCompra] = useState('');
+  const [precioVenta, setPrecioVenta] = useState('');
+  const [descuento, setDescuento] = useState('');
+  const [fechaDescuento, setFechaDescuento] = useState('');
+  const [estadoDescuento, setEstadoDescuento] = useState('');
 
- const handleSubmit = () => {
-    // Implement your API call or form submission logic here
-    Alert.alert('Form submitted!', JSON.stringify({ producto, precioCompra, precioVenta, descuento, fechaDescuento, estadoDescuento }));
- };
+  const handleSubmit = () => {
+    // Implementa tu lógica de llamada a la API o envío de formulario aquí
+    Alert.alert(
+      'Formulario enviado!',
+      JSON.stringify({
+        producto,
+        precioCompra,
+        precioVenta,
+        descuento,
+        fechaDescuento,
+        estadoDescuento,
+      })
+    );
+  };
 
- return (
+  return (
     <ScrollView style={styles.container}>
       <Text style={styles.title}>Registrar precio</Text>
 
       <TextInput
         style={styles.input}
         placeholder="ID Producto"
-        onChangeText={text => setProducto(text)}
+        onChangeText={(text) => setProducto(text)}
         value={producto}
       />
 
       <TextInput
         style={styles.input}
         placeholder="Precio compra"
-        onChangeText={text => setPrecioCompra(text)}
+        onChangeText={(text) => setPrecioCompra(text)}
         value={precioCompra}
       />
 
       <TextInput
         style={styles.input}
         placeholder="Precio venta"
-        onChangeText={text => setPrecioVenta(text)}
+        onChangeText={(text) => setPrecioVenta(text)}
         value={precioVenta}
       />
 
       <TextInput
         style={styles.input}
         placeholder="Descuento"
-        onChangeText={text => setDescuento(text)}
+        onChangeText={(text) => setDescuento(text)}
         value={descuento}
       />
 
       <TextInput
         style={styles.input}
         placeholder="Fecha de descuento"
-        onChangeText={text => setFechaDescuento(text)}
+        onChangeText={(text) => setFechaDescuento(text)}
         value={fechaDescuento}
       />
 
       <View style={styles.dropdown}>
         <Picker
           selectedValue={estadoDescuento}
-          onValueChange={(itemValue, itemIndex) => setEstadoDescuento(itemValue)}
+          onValueChange={(itemValue, itemIndex) =>
+            setEstadoDescuento(itemValue)
+          }
         >
           <Picker.Item label="Activo" value="Activo" />
           <Picker.Item label="Inactivo" value="Inactivo" />
@@ -76,42 +89,42 @@ const RegistrarPrecio = () => {
         <Text style={styles.buttonText}>Registrar</Text>
       </TouchableOpacity>
     </ScrollView>
- );
+  );
 };
 
 const styles = StyleSheet.create({
- container: {
+  container: {
     padding: 20,
- },
- title: {
+  },
+  title: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
- },
- input: {
+  },
+  input: {
     borderWidth: 1,
     borderColor: 'grey',
     borderRadius: 5,
     padding: 10,
     marginBottom: 10,
- },
- dropdown: {
+  },
+  dropdown: {
     borderWidth: 1,
     borderColor: 'grey',
     borderRadius: 5,
     padding: 10,
     marginBottom: 10,
- },
- button: {
+  },
+  button: {
     backgroundColor: 'blue',
     padding: 10,
     borderRadius: 5,
- },
- buttonText: {
+  },
+  buttonText: {
     color: 'white',
     fontSize: 18,
     textAlign: 'center',
- },
+  },
 });
 
 export default RegistrarPrecio;
