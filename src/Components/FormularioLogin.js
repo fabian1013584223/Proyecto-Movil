d882bcb31d4be24fdd8e149521926e660322b25c
fabian1013584223 <114703051+fabian1013584223@users.noreply.github.com>
2023-11-30 09:40:24 -0500

    Correción error linea 11

--- src/Components/FormularioLogin.js
+++ src/Components/FormularioLogin.js
@@ -1,26 +1,27 @@
 import React, { useState } from 'react';
 import {
- View,
- Text,
- StyleSheet,
- TextInput,
- Picker,
- TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Picker,
+  TouchableOpacity,
 } from 'react-native';
 
-const RegistrationForm = () => {
- const [selectedDocType, setSelectedDocType] = useState('');
- const [name, setName] = useState('');
- const [lastName, setLastName] = useState('');
- const [email, setEmail] = useState('');
- const [phoneNumber, setPhoneNumber] = useState('');
+const FormularioLogin = () => {
+  const [selectedDocType, setSelectedDocType] = useState('');
+  const [documentNumber, setDocumentNumber] = useState('');
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
 
- const handleRegistration = () => {
-    // Implement your registration logic here
-    console.log('Registration Successful');
- };
+  const handleRegistration = () => {
+    // Implementa tu lógica de registro aquí
+    console.log('Registro exitoso');
+  };
 
- return (
+  return (
     <View style={styles.container}>
       <Text style={styles.label}>Tipo de documento</Text>
       <Picker
@@ -28,13 +29,13 @@ const RegistrationForm = () => {
         onValueChange={(itemValue, itemIndex) => setSelectedDocType(itemValue)}
         style={styles.picker}
       >
-        <Picker.Item label="Identity Card" value="identity_card" />
-        <Picker.Item label="Citizenship Card" value="citizenship_card" />
-        <Picker.Item label="Immigration Card" value="immigration_card" />
-        <Picker.Item label="Passport" value="passport" />
+        <Picker.Item label="Cédula de identidad" value="identity_card" />
+        <Picker.Item label="Carné de ciudadanía" value="citizenship_card" />
+        <Picker.Item label="Carné de inmigración" value="immigration_card" />
+        <Picker.Item label="Pasaporte" value="passport" />
       </Picker>
 
-      <Text style={styles.label}>Documento</Text>
+      <Text style={styles.label}>Número de documento</Text>
       <TextInput
         style={styles.input}
         onChangeText={setDocumentNumber}
@@ -73,39 +74,39 @@ const RegistrationForm = () => {
         <Text style={styles.buttonText}>Registrarse</Text>
       </TouchableOpacity>
     </View>
- );
+  );
 };
 
 const styles = StyleSheet.create({
- container: {
+  container: {
     flex: 1,
     padding: 20,
- },
- label: {
+  },
+  label: {
     fontSize: 18,
     marginBottom: 5,
- },
- input: {
+  },
+  input: {
     borderWidth: 1,
     borderColor: 'black',
     padding: 10,
     marginBottom: 20,
- },
- picker: {
+  },
+  picker: {
     height: 50,
     borderWidth: 1,
     borderColor: 'black',
- },
- button: {
+  },
+  button: {
     backgroundColor: 'blue',
     padding: 10,
     borderRadius: 5,
- },
- buttonText: {
+  },
+  buttonText: {
     color: 'white',
     fontSize: 18,
     textAlign: 'center',
- },
+  },
 });
 
 export default FormularioLogin;
