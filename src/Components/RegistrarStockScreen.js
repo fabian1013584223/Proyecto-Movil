9b7c7a07666df063d777f2b7cb9267f40dc20e73
fabian1013584223 <114703051+fabian1013584223@users.noreply.github.com>
2023-12-12 16:21:23 -0500

    Correcion Formulario registro stock

--- src/Components/RegistrarStockScreen.js
+++ src/Components/RegistrarStockScreen.js
@@ -1,29 +1,15 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, DatePickerAndroid, Platform, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 const App = () => {
   const [idStock, setIdStock] = useState('');
   const [idProducto, setIdProducto] = useState('');
+  const [nombreProducto, setNombreProducto] = useState('');
   const [cantidadReal, setCantidadReal] = useState('');
   const [cantidadIdeal, setCantidadIdeal] = useState('');
   const [cantidadMinima, setCantidadMinima] = useState('');
   const [cantidadAlarma, setCantidadAlarma] = useState('');
-  const [fechaIngreso, setFechaIngreso] = useState(null);
-
-  const handleDateSelection = async () => {
-    try {
-      if (Platform.OS === 'android') {
-        const { action, year, month, day } = await DatePickerAndroid.open({
-          date: new Date(),
-        });
-        if (action !== DatePickerAndroid.dismissedAction) {
-          setFechaIngreso(new Date(year, month, day));
-        }
-      }
-    } catch (error) {
-      console.warn('Cannot open date picker', error.message);
-    }
-  };
+  const [fechaIngreso, setFechaIngreso] = useState('');
 
   const handleRegistrar = () => {
     // Lógica para registrar
@@ -36,11 +22,26 @@ const App = () => {
 
   return (
     <View style={styles.container}>
+      <h1>Registrar Stock</h1>
+      <Text style={styles.label}>ID Stock:</Text>
       <TextInput style={styles.input} placeholder="ID Stock" onChangeText={(text) => setIdStock(text)} />
+
+      <Text style={styles.label}>ID Producto:</Text>
       <TextInput style={styles.input} placeholder="ID Producto" onChangeText={(text) => setIdProducto(text)} />
+
+      <Text style={styles.label}>Nombre del Producto:</Text>
+      <TextInput style={styles.input} placeholder="Nombre del Producto" onChangeText={(text) => setNombreProducto(text)} />
+
+      <Text style={styles.label}>Cantidad Real:</Text>
       <TextInput style={styles.input} placeholder="Cantidad real" onChangeText={(text) => setCantidadReal(text)} />
+
+      <Text style={styles.label}>Cantidad Ideal:</Text>
       <TextInput style={styles.input} placeholder="Cantidad ideal" onChangeText={(text) => setCantidadIdeal(text)} />
+
+      <Text style={styles.label}>Cantidad Mínima:</Text>
       <TextInput style={styles.input} placeholder="Cantidad mínima" onChangeText={(text) => setCantidadMinima(text)} />
+
+      <Text style={styles.label}>Cantidad de Alarma:</Text>
       <TextInput style={styles.input} placeholder="Cantidad de alarma" onChangeText={(text) => setCantidadAlarma(text)} />
 
       <View style={styles.dateContainer}>
@@ -48,14 +49,16 @@ const App = () => {
         <TextInput
           style={styles.dateInput}
           placeholder="DD/MM/AAAA"
-          value={fechaIngreso ? fechaIngreso.toLocaleDateString() : ''}
-          onFocus={handleDateSelection}
+          value={fechaIngreso}
+          onChangeText={(text) => setFechaIngreso(text)}
         />
       </View>
 
-      <Button title="Registrar" onPress={handleRegistrar} />
-
-      <Button title="Cancelar Registro" onPress={handleCancelarRegistro} />
+      <View style={styles.buttonContainer}>
+        <Button title="Registrar" onPress={handleRegistrar} color="#1565C0" />
+        <View style={{ height: 10 }} /> {/* Espacio entre los botones */}
+        <Button title="Cancelar Registro" onPress={handleCancelarRegistro} color="#1565C0" />
+      </View>
     </View>
   );
 };
@@ -70,16 +73,20 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     padding: 10,
   },
+  label: {
+    marginBottom: 11,
+    color: '#1565C0',
+  },
   dateContainer: {
     marginBottom: 10,
-  },
-  label: {
-    marginBottom: 5,
   },
   dateInput: {
     borderWidth: 1,
     borderColor: 'grey',
     padding: 10,
+  },
+  buttonContainer: {
+    marginTop: 10,
   },
 });
 
