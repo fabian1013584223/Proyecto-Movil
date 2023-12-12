514ba34588bdb8c760b786da79eb2a82b7009309
fabian1013584223 <114703051+fabian1013584223@users.noreply.github.com>
2023-12-12 09:00:59 -0500

    Vistas

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import FormularioLogin from './src/Components/FormularioLogin';
-// import ModificarCliente from './src/Components/ModificarCliente';
+import RegistrarStockScreen from './src/Components/RegistrarStockScreen';
+import RegistrarPrecioScreen from './src/Components/RegistrarPrecioScreen';
+import RegistrarproducScreen from './src/Components/RegistrarproducScreen';
+import RgstroProveedorScreen from './src/Components/RgstroProveedorScreen';
+import ModificarCliente from './src/Components/ModificarCliente';
 
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Registro de Usuario</Text>
-      {/* <Text style={styles.title}>Modificar Cliente</Text> */}
+      <Text style={styles.title}>Registrar Stock</Text> 
+      <Text style={styles.title}>Registrar Precio</Text>
+      <Text style={styles.title}>Registrar Producto</Text>
+      <Text style={styles.title}>Registrar Proveedores</Text>
+      <Text style={styles.title}>Modificar Cliente</Text> 
+      <Text style={styles.title}>Modificar Precio</Text> 
       <FormularioLogin />
-      {/* <ModificarCliente /> */}
+      <RegistrarStockScreen/>
+      <RegistrarPrecioScreen/>
+      <RegistrarproducScreen/>
+      <RgstroProveedorScreen/>
+      <ModificarCliente />
+      <ModificarPrecio/>
     </View>
   );
 };
